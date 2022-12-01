2f120ae1891e9eeea4b998c97497546520d284a8
agustinestrada <agustindeestrada91@gmail.com>
2022-12-01 14:18:04 -0300

    app

---
+++ app.js
@@ -0,0 +1,103 @@
+let cantidadDeComensales = parseInt(prompt('Introduce el total de las personas'))
+let participantes = []
+let totalDivision = 0
+let selector = document.getElementById('select-persona')
+let personas = []
+let calcular = document.querySelector('button.calcular')
+let tablaComensales = document.getElementById('tablaid')
+let sumaTotal = 0
+let division
+let saldo = document.querySelector('div.saldo')
+
+
+// Inputs
+let nombre = document.querySelector('select#select-persona')
+let carga = document.querySelector('input#aporte')
+
+class Comensal {
+    constructor(nombre) {  
+      this.nombre = nombre,
+      this.aporte = 0,
+      this.saldito = 0
+      
+    }
+  }
+
+while (isNaN(cantidadDeComensales)) {
+    cantidadDeComensales = parseInt(prompt('Introduce el total de las personas'))
+}
+
+for (let i = 1; i <= cantidadDeComensales; i++) {
+    participantes.push(prompt(`Ingrese el nombre del comensal ${i}`))  
+}
+
+for (let i = 0; i < participantes.length ; i++) {
+    selector.options[i] = new Option(`${participantes[i]}`,`${participantes[i]}`)
+    personas.push(new Comensal(participantes[i]))
+}
+
+let formulario = document.querySelector('form.inputs')
+
+
+function crearTabla(lista) {
+    let stringTabla = "<tr><th>Nombre</th><th>Aporte Total</th><th>Saldo</th></tr>"
+    
+    personas.forEach(persona => {
+        let fila = "<tr> <td>"
+        fila += persona.nombre
+        fila += "</td>"
+        
+        fila += "<td>"
+        fila += persona.aporte
+        fila += "</td>"
+
+        fila += "<td>"
+        fila += persona.saldito
+        fila += "</td>"
+        
+        fila += "</tr>"
+        
+        stringTabla += fila
+        
+        //console.log(stringTabla);
+    });
+    return stringTabla
+}
+
+formulario.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    let aportante = personas.find(i => i.nombre == nombre.value)
+
+    //console.log(aportante);
+    aportante.aporte += parseInt(carga.value)
+
+    tablaComensales.innerHTML = crearTabla(personas)
+    
+    personas.forEach(persona =>{
+        sumaTotal += persona.aporte
+    
+    })
+    
+
+})
+
+calcular.addEventListener('click',() => {
+    
+    if (personas[0].saldito != 0) {
+        alert('Por favor vuelva a cargar la pagina')
+    }else{
+        division = Math.floor(sumaTotal/cantidadDeComensales)
+        saldo.innerHTML = `Son ${division} por pera`
+        
+        personas.forEach(persona =>{
+            persona.saldito += division-persona.aporte
+        })
+        
+        tablaComensales.innerHTML = crearTabla(personas)
+    }
+
+    
+})
+
+tablaComensales.innerHTML = crearTabla(personas)
